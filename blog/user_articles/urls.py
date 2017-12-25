acdcd86f8abf1777e60221d6f629579333ed0c4b
bibi <670839606@qq.com>
2017-12-25 11:30:07 +0800

    主页点进去可以看文章内容了click+1

--- blog/user_articles/urls.py
+++ blog/user_articles/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     url(r'^index_(\d+)_(\d+)/$',views.index),
     url(r'^search_(\d+)/$',views.search),
     url(r'^article_(\d+)/$',views.detail),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
--- blog/user_articles/views.py
+++ blog/user_articles/views.py
@@ -10,17 +10,19 @@ from django.db.models import Q
 def index(request,sort,pindex):
     hotest = Artical.objects.filter().order_by('-click')[0:3]
     article_list = []
-    if sort == 1:
+    if sort == '1':
         article_list = Artical.objects.filter().order_by('-modifydate')
-    elif sort == 2:
+        print(article_list)
+    elif sort == '2':
         article_list = Artical.objects.filter().order_by('-click')
+        print(article_list)
     paginator = Paginator(article_list, 10)
     page = paginator.page(int(pindex))
-    maxpage = len(article_list)/10
+    maxpage = int(len(article_list)/10)+1
     context = {
         'page':page,
-        'paginator':paginator,
         'hotest':hotest,
+        'sort':sort,
         'pindex':pindex,
         'maxpage':maxpage,
     }
@@ -28,7 +30,10 @@ def index(request,sort,pindex):
 
 def detail(request,aid):
     article = Artical.objects.get(pk=int(aid))
-    author =UserInfo.objects.get(pk=int(article.uid))
+    user = article.uid
+    author =UserInfo.objects.get(pk=int(user.id))
+    article.click = article.click + 1
+    article.save()
     context = {
         'article':article,
         'author':author,

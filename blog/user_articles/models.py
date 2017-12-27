228426a4aac934c69a3980cc5fd9e96e6a49f5ca
bibi <670839606@qq.com>
2017-12-27 16:52:20 +0800

    修复时区不对问题

--- blog/user_articles/models.py
+++ blog/user_articles/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from tinymce.models import HTMLField
 
 class Artical(models.Model):
-    title = models.CharField(max_length=20)
+    title = models.CharField(max_length=40)
     content = HTMLField()
     click = models.IntegerField()
     createdate = models.DateTimeField(auto_now_add=True)
